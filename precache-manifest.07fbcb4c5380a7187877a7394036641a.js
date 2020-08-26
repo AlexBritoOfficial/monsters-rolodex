b6e61f8f842561024adfea49be284182e7e1a39e
Brito <britoa@wit.edu>
2020-08-26 18:52:01 -0400

    Updates

---
+++ precache-manifest.07fbcb4c5380a7187877a7394036641a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacdd326fa3f615c9ee948202ad05fb2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85f9192efce10ee4b612",
+    "url": "/monsters-rolodex/static/css/main.bc7a722d.chunk.css"
+  },
+  {
+    "revision": "fbd81bc24a986afdb69b",
+    "url": "/monsters-rolodex/static/js/2.219e708e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.219e708e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f9192efce10ee4b612",
+    "url": "/monsters-rolodex/static/js/main.4748885a.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
